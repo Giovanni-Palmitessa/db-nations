8bc0c65cf8dd98e778531849e31a0434f3f71cbb
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-03 14:26:56 +0100

    Add string username

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -3,6 +3,10 @@ package org.lessons.java;
 public class Main {
     public static void main(String[] args) {
         // Parametri di Connessione
+
+        // URL Database
         String url = "jdbc:mysql://localhost:3306/db_nations";
+        // Username
+        String user = "root";
     }
 }
