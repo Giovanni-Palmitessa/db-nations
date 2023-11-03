f8c16f7dd0a393f7cd3fa2d2e8983dd56f8c8b84
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-03 14:23:23 +0100

    Add string url

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -2,6 +2,7 @@ package org.lessons.java;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Parametri di Connessione
+        String url = "jdbc:mysql://localhost:3306/db_nations";
     }
 }
