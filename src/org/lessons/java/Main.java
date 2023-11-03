e344b92dbcfba20070502cfb4dc6a280f9324c12
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-03 14:32:08 +0100

    Add try catch for statement

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -2,6 +2,7 @@ package org.lessons.java;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Main {
@@ -17,7 +18,14 @@ public class Main {
 
         //provo ad aprire una connessione con try with resources
         try(Connection connection = DriverManager.getConnection(url, user, password)) {
+            String query = "";
+            //la connection prepara uno statement sql
+            try(PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
+            } catch (SQLException e) {
+                System.out.println("Unable to prepare statement");
+                e.printStackTrace();
+            }
         } catch (SQLException e) {
             System.out.println("Unable to open connection!");
             e.printStackTrace();
